0c8667ab2775d32d7836bba54ffff6d3b60f9ec7
rv.zelenskiy <zelnskiroman@gmail.com>
2023-06-30 18:43:41 +0300

    dssdv

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,8 @@
-import { useState } from "react";
 import "./App.css";
 import MainPage from "./pages";
-import BaseList from "./components/List";
-import VoteItem from "./components/VoteItem";
 
 function App() {
-  return (
-    <div style={ {display: 'flex'} }>
-      <div className="navigation__panel">
-        <BaseList ItemTemplate={VoteItem} source={{url: ''}} onItemClick={() => {}}/>
-      </div>
-      <MainPage />
-    </div>
-  );
+  return (<MainPage />);
 }
 
 export default App;
