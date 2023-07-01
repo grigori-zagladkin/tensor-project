000d9460baeba6ccf903ffcca73e46e22033efb4
grigory zagladkin <grigorizagladkin@gmail.com>
2023-07-01 01:23:49 -0700

    "aewfeferferfgreg"

--- client/src/components/Layout/index.tsx
+++ client/src/components/Layout/index.tsx
@@ -6,21 +6,22 @@ import Main from "../Main";
 import ThemeItem from "../ThemeItem";
 import { ITheme } from "../../types/theme.interface";
 
-const Layout: FC<PropsWithChildren> = ({ children }) => {
+const Layout: FC = () => {
   const [selectedTopic, selectTopic] = useState(null);
   const [selectedTheme, selectTheme] = useState(null);
-
-
 
   return (
     <div
       style={{
         display: "flex",
-        width: '100%'
-      }}>
-      <div className='naviagtion_container' style={{display: 'flex'}}>
+        width: "100%",
+      }}
+    >
+      <div className="naviagtion_container" style={{ display: "flex" }}>
         <BaseList
-          className={`navigation__list ${selectedTopic ? 'topic-selected' : ''}`}
+          className={`navigation__list ${
+            selectedTopic ? "topic-selected" : ""
+          }`}
           ItemTemplate={VoteItem}
           source={{
             url: "",
@@ -29,17 +30,24 @@ const Layout: FC<PropsWithChildren> = ({ children }) => {
             selectTopic(item as any);
           }}
         />
-        <div className={`navigation__topics__list navigation__list ${selectedTopic ? 'topic-selected' : ''}`}>
-          <Button caption="Назад" onClick={() => {
-            selectTopic(null);
-          }}/>
+        <div
+          className={`navigation__topics__list navigation__list ${
+            selectedTopic ? "topic-selected" : ""
+          }`}
+        >
+          <Button
+            caption="Назад"
+            onClick={() => {
+              selectTopic(null);
+            }}
+          />
           <BaseList
             ItemTemplate={ThemeItem}
             source={{
               url: "",
               filter: {
-                voteId: (selectTopic as any).id
-              }
+                voteId: (selectTopic as any).id,
+              },
             }}
             onItemClick={(items: object) => {
               selectTheme(items as any);
@@ -47,10 +55,23 @@ const Layout: FC<PropsWithChildren> = ({ children }) => {
           />
         </div>
       </div>
-      <div  className='workspace__container'>
-        <Main
-          theme={selectedTheme as any}
-        />
+      <div className="workspace__container">
+        {selectedTheme ? (
+          <Main theme={selectedTheme as any} />
+        ) : (
+          <div
+            style={{
+              fontSize: "30px",
+              fontWeight: "600",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "100vh",
+            }}
+          >
+            Выберите тему для голосования
+          </div>
+        )}
       </div>
     </div>
   );
